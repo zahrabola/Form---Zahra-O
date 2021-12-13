bdeab764f085ed44f146db0437c12607bc69419b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2021-12-13 18:55:07 +0000

    Update main.js
    add java script

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-1;
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordcheck = document.getElementById("passwordcheck");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const passwordcheckValue = passwordcheck.value.trim();
+
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  } else {
+    setSuccessFor(username);
+  }
+}
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = message;
+  formControl.className = "form-control error";
+}
+
